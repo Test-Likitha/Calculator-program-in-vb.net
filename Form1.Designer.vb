76ee45f102c395537edc4feccf5fb30d08cf068c
Lenovo <Lenovo@LIKITHA>
2022-02-24 12:58:10 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,291 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.num7 = New System.Windows.Forms.Button()
+        Me.num8 = New System.Windows.Forms.Button()
+        Me.num9 = New System.Windows.Forms.Button()
+        Me.addd = New System.Windows.Forms.Button()
+        Me.num4 = New System.Windows.Forms.Button()
+        Me.num6 = New System.Windows.Forms.Button()
+        Me.num5 = New System.Windows.Forms.Button()
+        Me.subt = New System.Windows.Forms.Button()
+        Me.num1 = New System.Windows.Forms.Button()
+        Me.num3 = New System.Windows.Forms.Button()
+        Me.num2 = New System.Windows.Forms.Button()
+        Me.mult = New System.Windows.Forms.Button()
+        Me.num0 = New System.Windows.Forms.Button()
+        Me.calc = New System.Windows.Forms.Button()
+        Me.numdecimal = New System.Windows.Forms.Button()
+        Me.display = New System.Windows.Forms.TextBox()
+        Me.div = New System.Windows.Forms.Button()
+        Me.modulas = New System.Windows.Forms.Button()
+        Me.backspace = New System.Windows.Forms.Button()
+        Me.clear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'num7
+        '
+        Me.num7.Location = New System.Drawing.Point(62, 154)
+        Me.num7.Name = "num7"
+        Me.num7.Size = New System.Drawing.Size(94, 29)
+        Me.num7.TabIndex = 0
+        Me.num7.Text = "7"
+        Me.num7.UseVisualStyleBackColor = True
+        '
+        'num8
+        '
+        Me.num8.Location = New System.Drawing.Point(184, 154)
+        Me.num8.Name = "num8"
+        Me.num8.Size = New System.Drawing.Size(94, 29)
+        Me.num8.TabIndex = 1
+        Me.num8.Text = "8"
+        Me.num8.UseVisualStyleBackColor = True
+        '
+        'num9
+        '
+        Me.num9.Location = New System.Drawing.Point(306, 154)
+        Me.num9.Name = "num9"
+        Me.num9.Size = New System.Drawing.Size(94, 29)
+        Me.num9.TabIndex = 2
+        Me.num9.Text = "9"
+        Me.num9.UseVisualStyleBackColor = True
+        '
+        'addd
+        '
+        Me.addd.ForeColor = System.Drawing.Color.Red
+        Me.addd.Location = New System.Drawing.Point(429, 94)
+        Me.addd.Name = "addd"
+        Me.addd.Size = New System.Drawing.Size(94, 29)
+        Me.addd.TabIndex = 3
+        Me.addd.Text = "+"
+        Me.addd.UseVisualStyleBackColor = True
+        '
+        'num4
+        '
+        Me.num4.Location = New System.Drawing.Point(62, 211)
+        Me.num4.Name = "num4"
+        Me.num4.Size = New System.Drawing.Size(94, 29)
+        Me.num4.TabIndex = 7
+        Me.num4.Text = "4"
+        Me.num4.UseVisualStyleBackColor = True
+        '
+        'num6
+        '
+        Me.num6.Location = New System.Drawing.Point(306, 211)
+        Me.num6.Name = "num6"
+        Me.num6.Size = New System.Drawing.Size(94, 29)
+        Me.num6.TabIndex = 6
+        Me.num6.Text = "6"
+        Me.num6.UseVisualStyleBackColor = True
+        '
+        'num5
+        '
+        Me.num5.Location = New System.Drawing.Point(184, 211)
+        Me.num5.Name = "num5"
+        Me.num5.Size = New System.Drawing.Size(94, 29)
+        Me.num5.TabIndex = 5
+        Me.num5.Text = "5"
+        Me.num5.UseVisualStyleBackColor = True
+        '
+        'subt
+        '
+        Me.subt.ForeColor = System.Drawing.Color.Red
+        Me.subt.Location = New System.Drawing.Point(429, 154)
+        Me.subt.Name = "subt"
+        Me.subt.Size = New System.Drawing.Size(94, 29)
+        Me.subt.TabIndex = 4
+        Me.subt.Text = "-"
+        Me.subt.UseVisualStyleBackColor = True
+        '
+        'num1
+        '
+        Me.num1.Location = New System.Drawing.Point(62, 266)
+        Me.num1.Name = "num1"
+        Me.num1.Size = New System.Drawing.Size(94, 29)
+        Me.num1.TabIndex = 11
+        Me.num1.Text = "1"
+        Me.num1.UseVisualStyleBackColor = True
+        '
+        'num3
+        '
+        Me.num3.Location = New System.Drawing.Point(306, 266)
+        Me.num3.Name = "num3"
+        Me.num3.Size = New System.Drawing.Size(94, 29)
+        Me.num3.TabIndex = 10
+        Me.num3.Text = "3"
+        Me.num3.UseVisualStyleBackColor = True
+        '
+        'num2
+        '
+        Me.num2.Location = New System.Drawing.Point(184, 266)
+        Me.num2.Name = "num2"
+        Me.num2.Size = New System.Drawing.Size(94, 29)
+        Me.num2.TabIndex = 9
+        Me.num2.Text = "2"
+        Me.num2.UseVisualStyleBackColor = True
+        '
+        'mult
+        '
+        Me.mult.ForeColor = System.Drawing.Color.Red
+        Me.mult.Location = New System.Drawing.Point(429, 211)
+        Me.mult.Name = "mult"
+        Me.mult.Size = New System.Drawing.Size(94, 29)
+        Me.mult.TabIndex = 8
+        Me.mult.Text = "*"
+        Me.mult.UseVisualStyleBackColor = True
+        '
+        'num0
+        '
+        Me.num0.Location = New System.Drawing.Point(62, 323)
+        Me.num0.Name = "num0"
+        Me.num0.Size = New System.Drawing.Size(94, 29)
+        Me.num0.TabIndex = 15
+        Me.num0.Text = "0"
+        Me.num0.UseVisualStyleBackColor = True
+        '
+        'calc
+        '
+        Me.calc.ForeColor = System.Drawing.Color.Red
+        Me.calc.Location = New System.Drawing.Point(306, 323)
+        Me.calc.Name = "calc"
+        Me.calc.Size = New System.Drawing.Size(217, 29)
+        Me.calc.TabIndex = 14
+        Me.calc.Text = "="
+        Me.calc.UseVisualStyleBackColor = True
+        '
+        'numdecimal
+        '
+        Me.numdecimal.ForeColor = System.Drawing.Color.Red
+        Me.numdecimal.Location = New System.Drawing.Point(184, 323)
+        Me.numdecimal.Name = "numdecimal"
+        Me.numdecimal.Size = New System.Drawing.Size(94, 29)
+        Me.numdecimal.TabIndex = 13
+        Me.numdecimal.Text = "."
+        Me.numdecimal.UseVisualStyleBackColor = True
+        '
+        'display
+        '
+        Me.display.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.display.Location = New System.Drawing.Point(62, 29)
+        Me.display.MaxLength = 20
+        Me.display.Name = "display"
+        Me.display.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.display.Size = New System.Drawing.Size(461, 34)
+        Me.display.TabIndex = 16
+        Me.display.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'div
+        '
+        Me.div.ForeColor = System.Drawing.Color.Red
+        Me.div.Location = New System.Drawing.Point(429, 266)
+        Me.div.Name = "div"
+        Me.div.Size = New System.Drawing.Size(94, 29)
+        Me.div.TabIndex = 17
+        Me.div.Text = "/"
+        Me.div.UseVisualStyleBackColor = True
+        '
+        'modulas
+        '
+        Me.modulas.ForeColor = System.Drawing.Color.Red
+        Me.modulas.Location = New System.Drawing.Point(306, 95)
+        Me.modulas.Name = "modulas"
+        Me.modulas.Size = New System.Drawing.Size(94, 29)
+        Me.modulas.TabIndex = 20
+        Me.modulas.Text = "%"
+        Me.modulas.UseVisualStyleBackColor = True
+        '
+        'backspace
+        '
+        Me.backspace.Font = New System.Drawing.Font("Wingdings", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.backspace.Location = New System.Drawing.Point(184, 95)
+        Me.backspace.Name = "backspace"
+        Me.backspace.Size = New System.Drawing.Size(94, 29)
+        Me.backspace.TabIndex = 19
+        Me.backspace.Text = ""
+        Me.backspace.UseVisualStyleBackColor = True
+        '
+        'clear
+        '
+        Me.clear.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.clear.Location = New System.Drawing.Point(62, 95)
+        Me.clear.Name = "clear"
+        Me.clear.Size = New System.Drawing.Size(94, 29)
+        Me.clear.TabIndex = 18
+        Me.clear.Text = "CLEAR"
+        Me.clear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(563, 388)
+        Me.Controls.Add(Me.modulas)
+        Me.Controls.Add(Me.backspace)
+        Me.Controls.Add(Me.clear)
+        Me.Controls.Add(Me.div)
+        Me.Controls.Add(Me.display)
+        Me.Controls.Add(Me.num0)
+        Me.Controls.Add(Me.calc)
+        Me.Controls.Add(Me.numdecimal)
+        Me.Controls.Add(Me.num1)
+        Me.Controls.Add(Me.num3)
+        Me.Controls.Add(Me.num2)
+        Me.Controls.Add(Me.mult)
+        Me.Controls.Add(Me.num4)
+        Me.Controls.Add(Me.num6)
+        Me.Controls.Add(Me.num5)
+        Me.Controls.Add(Me.subt)
+        Me.Controls.Add(Me.addd)
+        Me.Controls.Add(Me.num9)
+        Me.Controls.Add(Me.num8)
+        Me.Controls.Add(Me.num7)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents num7 As Button
+    Friend WithEvents num8 As Button
+    Friend WithEvents num9 As Button
+    Friend WithEvents addd As Button
+    Friend WithEvents num4 As Button
+    Friend WithEvents num6 As Button
+    Friend WithEvents num5 As Button
+    Friend WithEvents subt As Button
+    Friend WithEvents num1 As Button
+    Friend WithEvents num3 As Button
+    Friend WithEvents num2 As Button
+    Friend WithEvents mult As Button
+    Friend WithEvents num0 As Button
+    Friend WithEvents calc As Button
+    Friend WithEvents numdecimal As Button
+    Friend WithEvents display As TextBox
+    Friend WithEvents div As Button
+    Friend WithEvents modulas As Button
+    Friend WithEvents backspace As Button
+    Friend WithEvents clear As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,120 @@
+﻿Public Class Form1
+    'reading input from  buttons'
+    Private Sub button_click(sender As Object, e As EventArgs) Handles subt.Click, numdecimal.Click, num9.Click, num8.Click, num7.Click, num6.Click, num5.Click, num4.Click, num3.Click, num2.Click, num1.Click, num0.Click, mult.Click, modulas.Click, div.Click, clear.Click, calc.Click, backspace.Click, addd.Click
+        Dim button As Button = CType(sender, Button)
+        If (display.Text.Length < 20) Then
+            If button.Name = "num1" Then
+                display.Text = display.Text + "1"
+            End If
+            If button.Name = "num2" Then
+                display.Text = display.Text + "2"
+            End If
+            If button.Name = "num3" Then
+                display.Text = display.Text + "3"
+            End If
+            If button.Name = "num4" Then
+                display.Text = display.Text + "4"
+            End If
+            If button.Name = "num5" Then
+                display.Text = display.Text + "5"
+            End If
+            If button.Name = "num6" Then
+                display.Text = display.Text + "6"
+            End If
+            If button.Name = "num7" Then
+                display.Text = display.Text + "7"
+            End If
+            If button.Name = "num8" Then
+                display.Text = display.Text + "8"
+            End If
+            If button.Name = "num9" Then
+                display.Text = display.Text + "9"
+            End If
+            If button.Name = "num0" Then
+                display.Text = display.Text + "0"
+            End If
+            If button.Name = "numdecimal" Then
+                display.Text = display.Text + "."
+            End If
+        End If
+
+        If button.Name = "addd" Then
+            display.Text = display.Text + "+"
+        End If
+        If button.Name = "subt" Then
+            display.Text = display.Text + "-"
+        End If
+        If button.Name = "mult" Then
+            display.Text = display.Text + "*"
+        End If
+        If button.Name = "div" Then
+            display.Text = display.Text + "/"
+        End If
+        If button.Name = "modulas" Then
+            display.Text = display.Text + "%"
+        End If
+        If button.Name = "clear" Then
+            display.Text = ""
+        End If
+        If button.Name = "backspace" Then
+            If display.Text.Length > 0 Then
+                display.Text = display.Text.Remove(display.Text.Length - 1, 1)
+            End If
+            If display.Text = "" Then
+                display.Text = "0"
+            End If
+        End If
+        If button.Name = "calc" Then
+            Dim equation As String = display.Text
+            Try
+                Dim result = New DataTable().Compute(equation, Nothing)
+                If Double.IsInfinity(result) Or Double.IsNaN(result) Then
+                    display.Text = "Divide by zero error "
+                ElseIf display.Text.Length > 20 Then
+                    display.Text = "Too long to calculate"
+                Else
+                    display.Text = result.ToString.TrimEnd("0").TrimEnd(".")
+                End If
+            Catch ex As Exception
+                MsgBox("Invalid expression")
+            End Try
+        End If
+    End Sub
+
+    'To take only numbers and operators as input'
+    Private Sub display_keypress(sender As Object, e As KeyPressEventArgs) Handles display.KeyPress
+        number_only(e)
+    End Sub
+    Private Sub number_only(ByVal e As System.Windows.Forms.KeyPressEventArgs)
+        If (Asc(e.KeyChar) >= 48 And Asc(e.KeyChar) <= 57) Or Asc(e.KeyChar) = 46 Or Asc(e.KeyChar) = 8 Or Asc(e.KeyChar) = 13 Or Asc(e.KeyChar) = 43 Or Asc(e.KeyChar) = 45 Or Asc(e.KeyChar) = 42 Or Asc(e.KeyChar) = 47 Then
+            '(Asc(e.KeyChar) >= 48 And Asc(e.KeyChar) <= 57 //allow numbers
+            'Asc(e.KeyChar) = 46 //allow dot operator
+            'Asc(e.KeyChar) = 43,45,42,47 //allow +-*/ respectively
+            'Asc(e.KeyChar) = 8 //allow backspace
+            'Asc(e.KeyChar) = 13 //allow enter'
+        Else
+            e.Handled = True
+            MsgBox("Not a valid input type")
+        End If
+    End Sub
+
+    Private Sub display_TextChanged(sender As Object, e As KeyPressEventArgs) Handles display.KeyPress
+        If e.KeyChar = "=" Or e.KeyChar = Convert.ToChar(13) Then
+            e.KeyChar = ""
+            Try
+                Dim equation As String = display.Text
+                Dim result = New DataTable().Compute(equation, Nothing)
+                If Double.IsInfinity(result) Or Double.IsNaN(result) Then
+                    display.Text = "Divide by zero error "
+                ElseIf display.Text.Length > 20 Then
+                    display.Text = "Too long to calculate"
+                Else
+                    display.Text = result.ToString.TrimEnd("0").TrimEnd(".")
+                End If
+            Catch ex As Exception
+                MsgBox("Invalid expression")
+            End Try
+
+        End If
+    End Sub
+End Class
